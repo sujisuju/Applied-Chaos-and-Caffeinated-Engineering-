e092a9fee51beb0bf4d4e475ff4869e222bcb26c
sujisuju <sujanaferdousjannatul@gmail.com>
2026-06-06 18:51:13 +0600

    Gcms_analyzer.py

--- Gcms_analyzer.py
+++ Gcms_analyzer.py
@@ -157,6 +157,6 @@ if uploaded_image is not None:
                 st.download_button(label="📄 Download Quick Data Sheet (PDF)", data=bytes(pdf_quick), file_name="gcms_quick_data.pdf", mime="application/pdf")
             with c2:
                 pdf_detailed = generate_detailed_gc_pdf(report_text_lines)
-                st.download_button(label="📘 Download Detailed Understanding Report (PDF)", data=bytes(pdf_detailed), file_name="gcms_comprehensive_report.pdf", mime="application/pdf")
+                st.download_button(label="📘 Download Report (PDF)", data=bytes(pdf_detailed), file_name="gcms_comprehensive_report.pdf", mime="application/pdf")
 else:
     st.info("💡 Ready. Upload a screenshot of your Gas Chromatogram graph to run digitizing diagnostics.")
